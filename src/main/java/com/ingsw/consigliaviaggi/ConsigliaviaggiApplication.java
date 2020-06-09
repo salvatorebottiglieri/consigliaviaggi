444212f1871640d8244c6b7286cda249b0c6a8ef
Salvatore Bottiglieri <sasibottiglieri0@gmail.com>
2020-06-09 19:56:12 +0200

    Instanziazione delle dipendenze

---
+++ src/main/java/com/ingsw/consigliaviaggi/ConsigliaviaggiApplication.java
@@ -0,0 +1,13 @@
+package com.ingsw.consigliaviaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsigliaviaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsigliaviaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsw/consigliaviaggi/ConsigliaviaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsw.consigliaviaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsigliaviaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
