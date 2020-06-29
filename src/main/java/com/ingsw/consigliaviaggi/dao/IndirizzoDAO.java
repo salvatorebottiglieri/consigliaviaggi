3a38dcb9cff224060f195ab7746ee0a49bc54194
Salvatore Bottiglieri <sasibottiglieri0@gmail.com>
2020-06-29 15:54:01 +0200

    Ripristino file di enumarazione, erroneamente cancellati

--- src/main/java/com/ingsw/consigliaviaggi/dao/IndirizzoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.ingsw.consigliaviaggi.dao;
-
-import com.ingsw.consigliaviaggi.model.Indirizzo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IndirizzoDAO extends CrudRepository<Indirizzo, String> {
-
-
-}
---
+++ src/main/java/com/ingsw/consigliaviaggi/model/TipoRecensore.java
@@ -0,0 +1,9 @@
+package com.ingsw.consigliaviaggi.model;
+
+public enum TipoRecensore {
+
+    pessimo,
+    buono,
+    ottimo,
+    eccellente
+}
